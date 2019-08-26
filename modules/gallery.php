951c1fce87025e1336e99f2d69d9f2f85079274f
unknown <dungle160396@gmail.com>
2019-08-26 09:26:55 +0700

    [menu] add route menu

--- modules/gallery.php
+++ modules/gallery.php
@@ -1,4 +1,5 @@
 <?php
+
 if (!defined('TTH_SYSTEM')) { die('Please stop!'); }
 
 $date = new DateClass();
@@ -7,6 +8,8 @@ $stringObj = new String();
 //---------------[ box-wp BEGIN ]---------------------------
 $category_id = 0;
 $breadcrumb = '';
+$breadcrumb_home = '<a href="'. HOME_URL_LANG . '" title="' . $lgTxt_menu_home . '"><i class="fa fa-home" aria-hidden="true"></i></a>';
+$breadcrumb_category = $breadcrumb_menu_parent = $breadcrumb_menu = '';
 
 $db->table = "category";
 $db->condition = "is_active = 1 and slug = '".$slug_cat."'";
@@ -15,19 +18,34 @@ $db->limit = 1;
 $rows = $db->select();
 foreach ($rows as $row) {
 	$category_id = $row['category_id']+0;
-	
+	$breadcrumb_category = '<a href="' . HOME_URL_LANG . '/' . $slug_cat . '" title="' . stripslashes($row['name']) . '">' . stripslashes($row['name']) . '</a>';
 }
 
 if($id_menu > 0) {
 	$parent = 0;
-	$db->table = "gallery_menu";
-	$db->condition = "gallery_menu_id = " . $id_menu;
+	$db->table = "article_menu";
+	$db->condition = "article_menu = " . $id_menu;
 	$db->order = "";
 	$db->limit = 1;
 	$rows = $db->select();
 	if($db->RowCount>0) {
 		foreach ($rows as $row) {
-			
+			if ($parent == 0) {
+				$breadcrumb_menu_parent = '<a href="" title="' . stripslashes($row['name']) . '">' . stripslashes($row['name']) . '</a>';
+			} else {
+				$db->table = "article_menu";
+				$db->condition = "article_menu_id = ".$parent ;
+				$db->order = "";
+				$db->limit = 1;
+				$rows_parent = $db->select();
+				if ($db->RowCount > 0) {
+					foreach ($rows_parent as $row_parent) {
+						$breadcrumb_menu_parent = ' <i class="fa fa-caret-right" aria-hidden="true"></i> <a href="' . HOME_URL_LANG . '/' . $slug_cat . '/' . stripslashes($row_parent['slug']) . '" title="' . stripslashes($row_parent['name']) . '">' . stripslashes($row_parent['name']) . '</a>';
+						$breadcrumb_menu = ' <i class="fa fa-caret-right" aria-hidden="true"></i> <a href="' . HOME_URL_LANG . '/' . $slug_cat . '/' . stripslashes($row['slug']) . '" title="' . stripslashes($row['name']) . '">' . stripslashes($row['name']) . '</a>';
+						$photo_avt = '<img src="'.HOME_URL_LANG.'/upload/article_menu/hinh-'.$row_parent['img'].'">';
+					}
+				}
+			}
 		}
 	}
 }
@@ -37,7 +55,7 @@ if ($id_article > 0){
 	include(_F_TEMPLATES . DS . "show_gallery.php");
 } else if($id_menu <= 0) {
 	$loc = array();
-	$db->table = "gallery_menu";
+	$db->table = "article_menu";
 
 	$db->condition = "is_active = 1 and category_id = ".$category_id;
 	$db->order = "sort ASC";
@@ -45,12 +63,12 @@ if ($id_article > 0){
 	$rows = $db->select();
 	$stt=0;
 	foreach($rows as $row) {
-		$loc[$stt] = $row['gallery_menu_id'];
+		$loc[$stt] = $row['article_menu_id'];
 		$stt++;
 	}
 	$loc = implode(',',$loc);
-	$db->table = "gallery";
-	$db->condition = "is_active = 1 and gallery_menu_id IN (".$loc.")";
+	$db->table = "article";
+	$db->condition = "is_active = 1 and article_menu_id IN (".$loc.")";
 	$db->order = "created_time DESC";
 	$db->limit = "";
 	$rows = $db->select();
@@ -64,20 +82,20 @@ if ($id_article > 0){
 		if($page<=0) $page=1;
 		$start=($page-1)*$per_page;
 
-		$db->table = "gallery";
-		$db->condition = "is_active = 1 and gallery_menu_id IN (".$loc.")";
+		$db->table = "article";
+		$db->condition = "is_active = 1 and article_menu_id IN (".$loc.")";
 		$db->order = "created_time DESC";
 		$db->limit = $start.','.$per_page;
 		$rows = $db->select();
 
 		$i = 0;
 			include(_F_TEMPLATES . DS . "show_list_gallery.php");
-			// showPageNavigation($page, $total_pages,'/'.$slug_cat.'?p=');
+			showPageNavigation($page, $total_pages,'/'.$slug_cat.'?p=');
 	}
 	else if ($total==1) {
 		$id = 0;
 		foreach($rows as $row) {
-			$id = $row['gallery_id'];
+			$id = $row['article_id'];
 		}
 		include(_F_TEMPLATES . DS . "show_gallery.php");
 	}
@@ -86,8 +104,8 @@ if ($id_article > 0){
 } else {
 	$slug_submenu = "";
 	$parent = false;
-	$db->table = "gallery_menu";
-	$db->condition = "is_active = 1 and gallery_menu_id = ".$id_menu;
+	$db->table = "article_menu";
+	$db->condition = "is_active = 1 and article_menu_id = ".$id_menu;
 	$db->order = "";
 	$db->limit = 1;
 	$rows = $db->select();
@@ -97,7 +115,7 @@ if ($id_article > 0){
 	}
 	if($parent) {
 		$loc = array();
-		$db->table = "gallery_menu";
+		$db->table = "article_menu";
 		$db->condition = "is_active = 1 and parent = ".$id_menu;
 		$db->order = "sort ASC";
 		$db->limit = "";
@@ -105,7 +123,7 @@ if ($id_article > 0){
 		$stt=0;
 		if($db->RowCount>0) {
 			foreach($rows as $row) {
-				$loc[$stt] = $row['gallery_menu_id'];
+				$loc[$stt] = $row['article_menu_id'];
 				$stt++;
 			}
 			$loc = implode(',',$loc);
@@ -117,8 +135,8 @@ if ($id_article > 0){
 		$loc = $id_menu;
 	}
 
-	$db->table = "gallery";
-	$db->condition = "is_active = 1 and gallery_menu_id IN (".$loc.")";
+	$db->table = "article";
+	$db->condition = "is_active = 1 and article_menu_id IN (".$loc.")";
 	$db->order = "created_time DESC";
 	$db->limit = "";
 	$rows = $db->select();
@@ -132,24 +150,22 @@ if ($id_article > 0){
 		if($page<=0) $page=1;
 		$start=($page-1)*$per_page;
 
-		$db->table = "gallery";
-		$db->condition = "is_active = 1 and gallery_menu_id IN (".$loc.")";
+		$db->table = "article";
+		$db->condition = "is_active = 1 and article_menu_id IN (".$loc.")";
 		$db->order = "created_time DESC";
 		$db->limit = $start.','.$per_page;
 		$rows = $db->select();
 
 		$i = 0;
 			include(_F_TEMPLATES . DS . "show_list_gallery.php");
-			// showPageNavigation($page, $total_pages,'/'.$slug_cat.'/'.$slug_submenu.'?p=');
+			showPageNavigation($page, $total_pages,'/'.$slug_cat.'/'.$slug_submenu.'?p=');
 	}
 	else if ($total==1) {
 		$id = 0;
 		foreach($rows as $row) {
-			$id = $row['gallery_id'];
+			$id = $row['article_id'];
 		}
 		include(_F_TEMPLATES . DS . "show_gallery.php");
 	}
 	else include(_F_MODULES . DS . "_updating.php");
 }
-echo '</div>';
-echo '</section>';
